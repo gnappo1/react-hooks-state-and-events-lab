750bc547aa5cbbfc75e06bd6ee3aaa1c05b16683
Matteo Piccini <gnappo.1@live.it>
2022-12-14 15:58:14 -0800

    Finish lab - does not include the fix on cart functionality

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { useState } from "react"
 
-function Item({ name, category }) {
+function Item({name, category, id, setCart}) {
+  const [inCart, setInCart] = useState(false);
+  
+  const item = {name, category, id}
+  const liClass = !inCart ? "" : "in-cart"
+  const buttonText = !inCart ? "Add To Cart" : "Remove From Cart"
+
+  const handleClick = (e) => {
+    if (!inCart) {
+      setCart(currentCart => [...currentCart, item])
+    } else {
+      setCart(currentCart => currentCart.filter(item => item.id !== id))
+    }
+    setInCart(currentValue => !currentValue)
+  }
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{buttonText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,41 @@
 import React from "react";
 import Item from "./Item";
+import {useState} from "react"
 
 function ShoppingList({ items }) {
+  const [cart, setCart] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  
+  const handleChange = (e) => {
+    // debugger
+    // what was typed? e.target.value
+    const newState = e.target.value
+    // say "hey state, you should now be what was typed"
+    setSelectedCategory(newState)
+  }
+  
+  // const filteredItems = items.filter(item => {
+  //   if (selectedCategory === "All") {
+  //     return item
+  //   }
+  //   return item.category === selectedCategory
+  // })
+
+  // const filteredItems = items.filter(item => selectedCategory === "All" ? item : item.category === selectedCategory)
+
+  const filteredItems = items.filter(item => item.category === selectedCategory)
+
+  const finalItems = selectedCategory === "All" ? items : filteredItems
+  
+  const itemComponentsList = finalItems.map((item) => (
+    <Item key={item.id} setCart={setCart} {...item} />
+  ))
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" value={selectedCategory} onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +43,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {itemComponentsList}
       </ul>
     </div>
   );
